2478c512ee890d2e373ea5f5f8afc210d291c7aa
jastka4 <justyna_skalska@o2.pl>
2017-04-02 03:28:45 +0200

    Add functionality to forms, input stored
    -created table for reservations
    -added opacity clas for disabled objects
    -modification of opacity based on input
    -checking people form for input
    -showing dates in another format
    -storing variables in reservation tab

--- js/main.js
+++ js/main.js
@@ -1,29 +1,52 @@
+$(function(){
+    var reservation = [];
+    
+    $('#map, #people, #hours, #summary').addClass('disabled');
+    
 /*===============================================================
 CALENDAR
 ===============================================================*/
-$(function(){
-        $('#datepicker').datepicker({
-            dateFormat: 'dd-mm-yy',
-            minDate: 0,
-            maxDate: '+2m',
-            showOtherMonths: true,
-            firstDay: 1,
-            dayNamesMin: ['Nd', 'Pn', 'Wt', 'Śr', 'Cz', 'Pt', 'Sb'],
-            monthNames: ['Styczeń', 'Luty', 'Marzec', 'Kwiecień', 'Maj', 'Czerwiec', 'Lipiec', 'Sierpień', 'Wrzesień', 'Październik', 'Listopad', 'Grudzień'],
-            onSelect: function(date){
-                var selected = $(this).val();
-                $(".date").html(selected);
-                }
-            });
-        });
-
+    $('#datepicker').datepicker({
+        dateFormat: 'mm-dd-yy',
+        minDate: 0,
+        maxDate: '+2m',
+        showOtherMonths: true,
+        firstDay: 1,
+        dayNamesMin: ['Nd', 'Pn', 'Wt', 'Śr', 'Cz', 'Pt', 'Sb'],
+        monthNames: ['Styczeń', 'Luty', 'Marzec', 'Kwiecień', 'Maj', 'Czerwiec', 'Lipiec', 'Sierpień', 'Wrzesień', 'Październik', 'Listopad', 'Grudzień'],
+        onSelect: function() {
+            var selected = $(this).val();
+            $(".date, .sum4").html(showDayName(selected));
+            $('#people').removeClass('disabled');
+            reservation[1] = selected.parseDate('dd-mm-yy');
+        }
+    });
 /*===============================================================
 CONVERSION TO DAY AND MONTH
 ===============================================================*/
-function showDayName(year, month, day){
-    var date = new Date(year, month, day);
-    var dayName = ["Niedziela", "Poniedziałek", "Wtorek", "Środa", "Czwartek", "Piątek", "Sobota"];
-    var monthName = ["stycznia", "lutego", "marca", "kwietnia", "maja", "czerwca", "lipca", "sierpnia", "września", "października", "listopada", "grudnia"];
-}
-        $('#hours .date').html('<strong>'+dayName[date.getDay()]+'</strong>, '
-                                +day + ' ' + monthName[month]);
+    var today = new Date();
+
+    function showDayName(selectedDate){
+        var date = new Date(selectedDate);
+        var dayName = ["Niedziela", "Poniedziałek", "Wtorek", "Środa", "Czwartek", "Piątek", "Sobota"];
+        var monthName = ["stycznia", "lutego", "marca", "kwietnia", "maja", "czerwca", "lipca", "sierpnia", "września", "października", "listopada", "grudnia"];
+
+        var changedDate = dayName[date.getDay()] + ",<br />" +  date.getDate() + " " + monthName[date.getMonth()];
+        return changedDate;
+    }
+    
+    $(".date").html(showDayName(today));
+    
+/*===============================================================
+NUMBER OF PEOPLE
+===============================================================*/
+    $("#numP").on('input', function(){
+        var numPeople = document.getElementById("numP").value;
+        if(numPeople >= 2 && numPeople <= 18){
+            $(".sum1").html(numPeople);
+            reservation[3] = numPeople;
+            $('#map').removeClass('disabled');
+        }
+        });
+
+});//end of the ready function
