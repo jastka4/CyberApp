f47898a7a9fc989b9eb1d8fc17052a3e99671469
Justyna Skalska <justyna_skalska@o2.pl>
2017-07-17 14:14:11 +0200

    Hours and tables selection works, summary updates

--- js/main.js
+++ js/main.js
@@ -1,6 +1,35 @@
-$(function() {
-    var reservation = [],
-        reserved = [];
+var reserved = [];
+res = {
+    available: true,
+    hours:
+    [ 
+        {
+            startHour: "1500",         //TODO: database structure???
+            endHour: "2030"
+        },
+        {
+            startHour: "2130",
+            endHour: "2300"
+        }
+    ]
+}
+for(let i = 0; i < 14; i++) {
+    reserved.push(res);
+}
+
+$(function () {                  //TODO:
+    var reservation = [];        //delete this, beacuse we use post method
+        //reserved = [];         //fix multiple hover (selecting both doesn't work)
+                                 //message on submit
+                                 //post - data will be re-submitted (the browser should alert the user that the data are about to be re-submitted)
+                                 //https://stackoverflow.com/questions/4295782/how-do-you-extract-post-data-in-node-js
+                                 //http://api.jquery.com/jQuery.ajax/
+                                 //walidacja posta (musi być wybrany przedział czasowy, a nie jedna godzina)
+
+    var IE = ((navigator.appName == 'Microsoft Internet Explorer'
+        ||  !!(navigator.userAgent.match(/Trident/)
+        || navigator.userAgent.match(/rv:11/))
+        || (typeof $.browser !== "undefined" && $.browser.msie == 1)) ? true : false) //check if IE (add Edge support)
 
     $('#map, #people, #hours, #summary').addClass('disabled');
 
@@ -14,11 +43,17 @@ $(function() {
         firstDay: 1,
         dayNamesMin: ['Nd', 'Pn', 'Wt', 'Śr', 'Cz', 'Pt', 'Sb'],
         monthNames: ['Styczeń', 'Luty', 'Marzec', 'Kwiecień', 'Maj', 'Czerwiec', 'Lipiec', 'Sierpień', 'Wrzesień', 'Październik', 'Listopad', 'Grudzień'],
-        onSelect: function() {
+        onSelect: function () {
             var selected = $(this).val();
             $(".date, .sum4").html(showDayName(selected));
             $('#people').removeClass('disabled');
             reservation[1] = selected; //change it to get day, month, year
+            
+            $("#numP").val("");
+            $('#map, #hours, #summary').addClass('disabled');
+            $(".sum1").html("");
+            resetHours();
+            resetTables();
         }
     });
     /*===============================================================
@@ -41,12 +76,14 @@ $(function() {
     ===============================================================*/
     var tableList = document.querySelectorAll("#map li");
 
-    $("#numP").on('change', function() {
+    $("#numP").on('input', function (event) {
         var numPeopleElement = document.getElementById("numP"),
             numPeople = Number(numPeopleElement.value);
+            resetHours();
+            resetTables();
         if (numPeople >= 2 && numPeople <= 20) {
             reservation[3] = parseInt(numPeople);
-            $(".sum1").html(reservation[3]);
+            $(".sum1").html(numPeople);
             $('#map').removeClass('disabled');
             showAvailableTables(numPeople);
             document.getElementById("errNum").innerHTML = numPeopleElement.validationMessage;
@@ -59,8 +96,20 @@ $(function() {
         }
     });
 
-    for (var i = 0; i< tableList.length; i++) {
-        tableList[i].onclick = markSelectedTable;
+    $("#numP").on('keydown', function(event) {
+        var e = event || window.event;
+        if((e.keyCode ? e.keyCode : e.which) === 13) {
+            e.preventDefault();
+        }
+    });
+
+    for (let i = 0; i < tableList.length; i++) {
+        tableList[i].addEventListener("click", function() {
+            resetHours();
+            markSelectedTable(tableList[i]);
+            if(tableList[i].classList.contains("selected"))
+                getReservedHours(i);
+        }, false);
     }
 
     function showAvailableTables(num) {
@@ -87,19 +136,19 @@ $(function() {
             case 5:
                 disableMultipleHover(tableList);
                 $(tableList).removeClass("available");
-				showAvailability(tableList[0]); //table1
-				showAvailability(tableList[1]); //table2
+                showAvailability(tableList[0]); //table1
+                showAvailability(tableList[1]); //table2
                 showAvailability(tableList[4]); //table5
-				showAvailability(tableList[6]); //table7
-				showAvailability(tableList[8]); //table9
+                showAvailability(tableList[6]); //table7
+                showAvailability(tableList[8]); //table9
                 break;
             case 6:
                 disableMultipleHover(tableList);
                 $(tableList).removeClass("available");
-				showAvailability(tableList[0]); //table1
-				showAvailability(tableList[1]); //table2
+                showAvailability(tableList[0]); //table1
+                showAvailability(tableList[1]); //table2
                 showAvailability(tableList[4]); //table5
-				showAvailability(tableList[8]); //table9
+                showAvailability(tableList[8]); //table9
                 break;
             case 7:
                 disableMultipleHover(tableList);
@@ -108,78 +157,77 @@ $(function() {
                 showAvailability(tableList[12]); //table13
                 showAvailability(tableList[13]); //table14
                 break;
-			case 8:
+            case 8:
                 disableMultipleHover(tableList);
-				$(tableList).removeClass("available");
+                $(tableList).removeClass("available");
                 enableMultipleHover(tableList[7], tableList[9]);
-				showAvailability(tableList[2]); //table3
-				showAvailability(tableList[3]); //table4
-				showAvailability(tableList[10]); //table11
-				showAvailability(tableList[11]); //table12
-				showAvailability(tableList[13]); //table14
-				showAvailability(tableList[14]); //table15
-				break;
-			case 9:
+                showAvailability(tableList[2]); //table3
+                showAvailability(tableList[3]); //table4
+                showAvailability(tableList[10]); //table11
+                showAvailability(tableList[11]); //table12
+                showAvailability(tableList[13]); //table14
+                break;
+            case 9:
                 disableMultipleHover(tableList);
-				$(tableList).removeClass("available");
-				showAvailability(tableList[10]); //table11
-				showAvailability(tableList[11]); //table12
-				break;
+                $(tableList).removeClass("available");
+                showAvailability(tableList[10]); //table11
+                showAvailability(tableList[11]); //table12
+                break;
             case 10:
                 disableMultipleHover(tableList);
-				$(tableList).removeClass("available");
+                $(tableList).removeClass("available");
                 enableMultipleHover(tableList[6], tableList[8]);
-				showAvailability(tableList[10]); //table11
-				showAvailability(tableList[11]); //table12
+                showAvailability(tableList[10]); //table11
+                showAvailability(tableList[11]); //table12
                 break;
             case 11:
                 disableMultipleHover(tableList);
-				$(tableList).removeClass("available");
+                $(tableList).removeClass("available");
                 enableMultipleHover(tableList[6], tableList[8]);
                 break;
             case 12:
                 disableMultipleHover(tableList);
-				$(tableList).removeClass("available");
+                $(tableList).removeClass("available");
                 enableMultipleHover(tableList[0], tableList[1]);
                 break;
             case 13:
                 disableMultipleHover(tableList);
-				$(tableList).removeClass("available");
+                $(tableList).removeClass("available");
                 enableMultipleHover(tableList[0], tableList[1]);
                 break;
             case 14:
                 disableMultipleHover(tableList);
-				$(tableList).removeClass("available");
+                $(tableList).removeClass("available");
                 enableMultipleHover(tableList[0], tableList[1]);
                 break;
             case 15:
                 disableMultipleHover(tableList);
-				$(tableList).removeClass("available");
+                $(tableList).removeClass("available");
                 enableMultipleHover(tableList[0], tableList[1]);
                 break;
             case 16:
                 disableMultipleHover(tableList);
-				$(tableList).removeClass("available");
+                $(tableList).removeClass("available");
                 enableMultipleHover(tableList[12], tableList[13]);
                 break;
             case 17:
                 disableMultipleHover(tableList);
-				$(tableList).removeClass("available");
+                $(tableList).removeClass("available");
                 enableMultipleHover(tableList[12], tableList[13]);
                 break;
             case 18:
                 disableMultipleHover(tableList);
-				$(tableList).removeClass("available");
+                $(tableList).removeClass("available");
                 enableMultipleHover(tableList[12], tableList[13]);
                 break;
             case 19:
                 disableMultipleHover(tableList);
-				$(tableList).removeClass("available");
+                $(tableList).removeClass("available");
                 enableMultipleHover(tableList[12], tableList[13]);
                 break;
             case 20:
                 disableMultipleHover(tableList);
-				$(tableList).removeClass("available");
+                $(tableList).removeClass("available");
                 enableMultipleHover(tableList[12], tableList[13]);
                 break;
             default:
@@ -187,83 +235,182 @@ $(function() {
         }
     }
 
-    function enableMultipleHover(table1, table2){
-        if(checkAvailability(table1) && checkAvailability(table2)){
+    function enableMultipleHover(table1, table2) {
+        if (checkAvailability(table1) && checkAvailability(table2)) {
             showAvailability(table1); //table13
-		    showAvailability(table2); //table14
-            $(table1).add(table2).hover(function() {
-                $(table1).addClass("multipleHover");
-                $(table2).addClass("multipleHover");
-            }, function() {
-                $(table1).removeClass("multipleHover");
-                $(table2).removeClass("multipleHover");
+            showAvailability(table2); //table14
+            $(table1).add(table2).hover(function () {
+                table1.classList.add("multipleHover");
+                table2.classList.add("multipleHover");
+            }, function () {
+                table1.classList.remove("multipleHover");
+                table2.classList.remove("multipleHover");
             });
         }
     }
 
-    function disableMultipleHover(table){
-        for(var i = 0; i < table.length; i++){
+    function disableMultipleHover(table) {
+        for (let i = 0; i < table.length; i++) {
             $(table[i]).off("mouseenter mouseleave");
         }
     }
 
-    function showAvailability(table){
-        if(checkAvailability(table)){
-            table.className += " available";
+    function showAvailability(table) {
+        if (checkAvailability(table)) {
+            table.classList.add("available");
         }
     }
 
     function checkAvailability(table) {
-        if(table.classList.contains("reserved")){
+        if (table.classList.contains("reserved")) {
             return false;
-        } else{
+        } else {
             return true;
         }
     }
 
-    function markSelectedTable(){
-        var siblings = $(this).siblings();
-        if(this.classList.contains("available")) {
-            if(this.classList.contains("selected")){
-                $(this).removeClass("selected");
+    function markSelectedTable(table) {
+        var siblings = $(table).siblings();
+        if (table.classList.contains("available")) {
+            if (table.classList.contains("selected")) {
+                table.classList.remove("selected");
                 $('#hours').addClass('disabled');
                 $(".sum2").html("");
             } else {
-                if(checkSelection(siblings)){
-                    for(var i = 0; i < siblings.length; i++){
-                        $(siblings[i]).removeClass("selected");
-                        this.className += " selected";
+                if (checkSelection(siblings)) {
+                    for (let i = 0; i < siblings.length; i++) {
+                        siblings[i].classList.remove("selected");
+                        table.classList.add("selected");
                     }
                 } else {
-                    this.className += " selected";
+                    table.classList.add("selected");
                     $('#hours').removeClass('disabled');
                 }
-                reservation[4] = parseInt(this.innerText);
+                reservation[4] = parseInt(table.innerText);
                 $(".sum2").html(reservation[4]);
             }
         }
     }
 
     function checkSelection(sib) {
-        for(var i = 0; i < sib.length; i++) {
-            if(sib[i].classList.contains("selected")) {
-               return true;
+        for (let i = 0; i < sib.length; i++) {
+            if (sib[i].classList.contains("selected")) {
+                return true;
             }
         }
 
         return false;
     }
-    
+
     /*===============================================================
     TABLES
     ===============================================================*/
-    for (var i = 1; i < 14; i++) {
+    for (let i = 1; i < 14; i++) {
         var table = document.getElementById("table" + i);
-        table.className += " free";
+        table.classList.add("free");
     }
-    for (var i = 14; i < 15; i++) {
+    for (let i = 14; i < 15; i++) {
         var table = document.getElementById("table" + i);
-        table.className += " reserved";
+        table.classList.add("reserved");
     }
 
+    function resetTables() {
+        for(let i = 0; i < tableList.length; i++) {
+            tableList[i].classList.remove("available", "selected");
+        }
+    }
+
+    /*===============================================================
+    HOURS PICKER
+    ===============================================================*/
+    $("#selectable").bind("mousedown", function (event) {
+        event.metaKey = true;
+    }).selectable({
+        filter: ".unbooked",
+        selected: function (event, ui) {
+            if (!ui.selected.classList.contains("selected-flag")) {
+                ui.selected.classList.add("selected-flag");                             //select
+            } else {
+                ui.selected.classList.remove("ui-selected", "selected-flag");           //deselect
+            }
+            //unselect remaining elements if the selected one doesn't boarder other previously selected elements 
+            if ((ui.selected.nextElementSibling ? !ui.selected.nextElementSibling.classList.contains("selected-flag") : true)
+                && (ui.selected.previousElementSibling ? !ui.selected.previousElementSibling.classList.contains("selected-flag") : true)) {
+                var siblings = $(ui.selected).siblings();
+                for (let i = 0; i < siblings.length; i++) {
+                    siblings[i].classList.remove("ui-selected", "selected-flag");
+                }
+            }
+        },
+        unselecting: function (event, ui) {  //unselect all elements that comes after the selected one if the selected one is between other selected elements
+            if ((ui.unselecting.nextElementSibling ? ui.unselecting.nextElementSibling.classList.contains("selected-flag") : false)
+                && (ui.unselecting.previousElementSibling ? ui.unselecting.previousElementSibling.classList.contains("selected-flag") : false)) {
+                var nextSiblings = $(ui.unselecting).nextAll();
+                for (let i = 0; i < nextSiblings.length; i++) {
+                    nextSiblings[i].classList.remove("ui-selected", "selected-flag");
+                }
+            }
+            if (ui.unselecting.classList.contains("selected-flag")) {
+                ui.unselecting.classList.remove("ui-selected", "selected-flag");            //deselect
+            }
+        },
+        stop: function(event, ui) {
+            var startHour = document.querySelector(".selected-flag").children[0].text,
+                allHours = document.querySelectorAll(".selected-flag"),
+                endHour = allHours[allHours.length-1].textContent;
+            if(startHour === endHour){
+                $(".sum3").html(startHour);
+            } else {
+                $(".sum3").html(startHour + " - " + endHour);
+            }
+            $("#summary").removeClass("disabled");
+        }
+    });
+
+    /*===============================================================
+    HOURS
+    ===============================================================*/
+    var $hours = $("#selectable").children();
+
+    function resetHours(){
+        for (var i = 0; i < $hours.length; i++) {
+            if($hours[i].classList.contains("booked")) {
+                $hours[i].classList.remove("booked");
+            }
+            $hours[i].classList.add("unbooked");
+            $hours[i].classList.remove("ui-selectee", "ui-selected", "selected-flag");
+        }
+    }
+
+    function getReservedHours(table){
+        for(let i = 0; i < reserved[table].hours.length; i++) {
+            //console.log("table " + table + " " + reserved[table].hours[i].startHour + "-" + reserved[table].hours[i].endHour);
+            showReservedHours(reserved[table].hours[i].startHour, reserved[table].hours[i].endHour);
+        }
+    }
+
+    function showReservedHours (start, end) {
+        var arr = $hours.filter(function(hour){
+            var cl = (this.classList)[0].split('-');
+            return parseInt(cl[1]) >= parseInt(start) && parseInt(cl[1]) <= parseInt(end);
+        })
+
+        for(let i = 0; i < arr.length; i++) {
+            arr[i].classList.remove("unbooked");
+            arr[i].classList.add("booked");
+        }
+    }
+    /*===============================================================
+    SEND RESERVATION INFO
+    ===============================================================*/
+    function loadReserved(day) {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.open("GET", "getReservation.php?q="+day, true); //add server file
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                //what to do with the response
+            }
+        }
+        xmlhttp.send();
+    }
 }); //end of the ready function
